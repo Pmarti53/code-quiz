c4c556d602d64bb571e8630f2e132a49602b9b01
peter <ptmartinezsound@gmail.com>
2021-03-11 17:34:07 -0600

    script start

--- assets/script.js
+++ assets/script.js
@@ -1,111 +1,116 @@
-// const start_btn = document.querySelector(".start-button")
-// const info_box = document.querySelector(".info-box");
-// const quiz_box = document.querySelector(".quiz_box");
-// const result_box = document.querySelector(".result_box");
-// const option_list = document.querySelector(".option_list");
-// const time_line = document.querySelector("header .time_line");
-// const timeText = document.querySelector(".timer .time_left_txt");
-// const timeCount = document.querySelector(".timer .timer_sec");
+var startButton = document.getElementById('start-btn')
 
-function start {
-   
+startButton.addEventListener('click', startQuiz)
+
+
+function startQuiz() {
+    console.log('Start')
+
 }
-//Questions
-let questions = [
-    {
-        number: 1,
-        question: "Question 1",
-        answer: "answer 3",
-        options: [
-            "answer 1",
-            "answer 2",
-            "answer 3",
-            "answer 4"
-        ]
-    },
 
-    {
-        number: 2,
-        question: "Question 2",
-        answer: "answer 1",
-        options: [
-            "answer 1",
-            "answer 2",
-            "answer 3",
-            "answer 4"
-        ]
-    },
+function nextQue() {
 
-    {
-        number: 3,
-        question: "Question 3",
-        answer: "answer 3",
-        options: [
-            "answer 1",
-            "answer 2",
-            "answer 3",
-            "answer 4"
-        ]
-    },
+}
 
-    {
-        number: 4,
-        question: "Question 4",
-        answer: "answer 2",
-        options: [
-            "answer 1",
-            "answer 2",
-            "answer 3",
-            "answer 4"
-        ]
-    },
+function selectAnswer() {
 
-    {
-        number: 5,
-        question: "Question 5",
-        answer: "answer 4",
-        options: [
-            "answer 1",
-            "answer 2",
-            "answer 3",
-            "answer 4"
-        ]
-    },
+}
+// //Questions
+// let questions = [
+//     {
+//         number: 1,
+//         question: "Question 1",
+//         answer: "answer 3",
+//         options: [
+//             "answer 1",
+//             "answer 2",
+//             "answer 3",
+//             "answer 4"
+//         ]
+//     },
 
-    {
-        number: 6,
-        question: "Question 6",
-        answer: "answer 1",
-        options: [
-            "answer 1",
-            "answer 2",
-            "answer 3",
-            "answer 4"
-        ]
-    },
+//     {
+//         number: 2,
+//         question: "Question 2",
+//         answer: "answer 1",
+//         options: [
+//             "answer 1",
+//             "answer 2",
+//             "answer 3",
+//             "answer 4"
+//         ]
+//     },
 
-    {
-        number: 7,
-        question: "Question 7",
-        answer: "answer 2",
-        options: [
-            "answer 1",
-            "answer 2",
-            "answer 3",
-            "answer 4"
-        ]
-    },
+//     {
+//         number: 3,
+//         question: "Question 3",
+//         answer: "answer 3",
+//         options: [
+//             "answer 1",
+//             "answer 2",
+//             "answer 3",
+//             "answer 4"
+//         ]
+//     },
 
-    {
-        number: 8,
-        question: "Question 1",
-        answer: "answer 4",
-        options: [
-            "answer 1",
-            "answer 2",
-            "answer 3",
-            "answer 4"
-        ]
-    },
+//     {
+//         number: 4,
+//         question: "Question 4",
+//         answer: "answer 2",
+//         options: [
+//             "answer 1",
+//             "answer 2",
+//             "answer 3",
+//             "answer 4"
+//         ]
+//     },
 
-]
+//     {
+//         number: 5,
+//         question: "Question 5",
+//         answer: "answer 4",
+//         options: [
+//             "answer 1",
+//             "answer 2",
+//             "answer 3",
+//             "answer 4"
+//         ]
+//     },
+
+//     {
+//         number: 6,
+//         question: "Question 6",
+//         answer: "answer 1",
+//         options: [
+//             "answer 1",
+//             "answer 2",
+//             "answer 3",
+//             "answer 4"
+//         ]
+//     },
+
+//     {
+//         number: 7,
+//         question: "Question 7",
+//         answer: "answer 2",
+//         options: [
+//             "answer 1",
+//             "answer 2",
+//             "answer 3",
+//             "answer 4"
+//         ]
+//     },
+
+//     {
+//         number: 8,
+//         question: "Question 1",
+//         answer: "answer 4",
+//         options: [
+//             "answer 1",
+//             "answer 2",
+//             "answer 3",
+//             "answer 4"
+//         ]
+//     },
+
+// ]

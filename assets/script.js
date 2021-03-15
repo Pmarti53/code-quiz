e462ea5589b9c962dc2a713d6f31e0b11bd780f8
peter <ptmartinezsound@gmail.com>
2021-03-14 20:05:20 -0500

    update

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,17 @@
 /**
- * click start button
+ *  click start button
  *  !-header element in info box is hidden as well as start button
- *  -!unhide question container element
+ *  !-unhide question container element
  *  !-start timer
  * 
  *  -make timer appear on page using html
- * quiz pops up
+ *  -quiz pops up
  * ! -make questions and answers appear
  *  
- * answer questions
- * -when answer is clicked, quiz goes to the next question and point is recorded\
- * -when answer is wrong, timer decreases
- * when quiz finishes, get score
+ *  answer questions
+ *  -when answer is clicked, quiz goes to the next question and point is recorded\
+ *  -when answer is wrong, timer decreases
+ *  -when quiz finishes, get score
  *  -save score to local storage
  *  -save initials
  */
@@ -21,12 +21,17 @@ var startButton = document.getElementById('start-btn');
 var quizIntro = document.getElementById('intro');
 var questionContainer = document.getElementById('question-container');
 var questionElement = document.getElementById('question');
-var answerButtons = document.getElementById('answer-buttons')
+var answerButtons = document.getElementById('answer-buttons');
 
 var shuffledQuestions, currentQuestionIndex
 
 
 startButton.addEventListener("click", startQuiz)
+answerButtons.addEventListener("click", () => {
+    currentQuestionIndex++
+    set
+})
+
 
 
 
@@ -35,51 +40,50 @@ function startQuiz() {
     startButton.classList.add('hide')
     questionContainer.classList.remove('hide')
     quizIntro.classList.add('hide')
-    shuffledQuestions = questions.sort(() => Math.random() -.5)
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     timer(90);
     nextQue
 };
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
 
-
-
+}
 
 
 function nextQue() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
-}
+};
 
-function showQuestion(question) {
-    questionElement.innerText = question.question
-}
 
-function selectAnswer() {
 
-}
+// function selectAnswer() {
+
+// }
 //Questions
-var questions = [
-    { 
+var questions = [{
         question: 'Question 1',
         answers: [
-            {text: '4', correct: true },
-            {text: '22', correct: false },
-            {text: '14', correct: false },
-            {text: '7', correct: false },
+            { text: '4', correct: true },
+            { text: '22', correct: false },
+            { text: '14', correct: false },
+            { text: '7', correct: false },
 
         ]
 
     }
 
 
-]
+];
 //timer function
 function timer(seconds) {
     var remaningTime = seconds;
-    window.setTimeout(function () {
+    window.setTimeout(function() {
         // console.log(remaningTime);
         if (remaningTime > 0) {
-            timer(remaningTime - 1,);
+            timer(remaningTime - 1, );
         }
     }, 1000);
 }
